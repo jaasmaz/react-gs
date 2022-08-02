a4451863d14518cdbb3d46be3a794a376a82d221
jaasmaz <jaasmaz@yahoo.com>
2022-08-02 13:35:21 +0300

    using rowIndex

--- src/genderField.jsx
+++ src/genderField.jsx
@@ -1,5 +1,6 @@
 export default (props) => {
   const value = props.value;
+  const rowIndex = props.rowIndex;
   return (
     <div style={{ textAlign: 'center' }}>
       <img
@@ -8,7 +9,11 @@ export default (props) => {
             ? 'https://cdn-icons-png.flaticon.com/512/64/64096.png'
             : 'https://cdn-icons-png.flaticon.com/512/65/65581.png'
         }
-        style={{ width: '25px' }}
+        style={{
+          width: '25px',
+          borderRadius: '3px',
+          background: rowIndex % 2 ? '#ffefef' : '#ffcccc',
+        }}
         alt={value}
       ></img>
     </div>
